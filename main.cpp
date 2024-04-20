c6a5f199b3b4aea37c0efd4c91c035e74ba149f8
unmeta <Willbydos@gmail.com>
2024-04-20 23:25:55 +1000

    Clean Code and added a proper menu system
    I cleaned it all :D, I'm pretty sure my Menu system doesn't work.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@
 
 using namespace std;
 
-// Function to display the content of a file
+/*
+*******************************
+*          SET UP             *
+*******************************
+*/
+// Function to display the content of a file (see in credits)
 
 void displayFileContent(const string & filename) {
     ifstream file(filename); // Open file with given filename for reading
@@ -23,7 +28,7 @@ void displayFileContent(const string & filename) {
   }
 }
 
-// Function to search for a string in a file and display line numbers where it is found
+// Function to search for a string in a file and display line numbers where it is found (see in credits)
 
 void searchAndDisplayLineNumbers(const string & filename, const string & searchStr) {
   ifstream file(filename); // Open file with given filename for reading
@@ -58,63 +63,114 @@ void searchAndDisplayLineNumbers(const string & filename, const string & searchS
   }
 }
 
-
+// main func etc yk
 int main(){
+    // Variables
     string usrname, pswd;
+    string website;
     bool check1 = true;
     string search;
     fstream myFile;
-    // Conformation and getting Usernames/Passwords
-    while (check1) {
-        char aswr = 'n';
-        char aswr2 = 'n';
-        
-        if (aswr == 'n') {
-            cout << "Type in Username: " << endl;
-            cin >> usrname;
-            cout << "Do you confirm this Username [y/n]?" << endl;
-            cin >> aswr;
-            if (aswr == 'y') {
-                cout << "Type in Password: " << endl;
-                cin >> pswd;
-                cout << "Do you confirm this Password [y/n]?" << endl;
-                cin >> aswr2;
-                if (aswr2 == 'y') {
-                    cout << "Thank you!" << endl;
-                    check1 = false;  // Exit the loop
-                } else {
-                    cout << "Please confirm the password again." << endl;
-                }
-            } else {
-                cout << "Please confirm the username again." << endl;
-            }
-        }
-    }
+    int choice;
 
-    printf("\ndgjhagh\n");
-
-    // File Handling
-
-    printf("\nabababab\n");
-    // Adding information into file
-    myFile.open("pswdHolder.txt", ios::app);
+    // Setting up Files
+        // Initual Adding to files.
+    myFile.open("pswdHolder.txt", ios::out);
     if(myFile.is_open()) {
-        myFile << usrname << ":" << pswd << endl;
+        myFile << "Password Mannager" << endl << "Website:Username:Password" << endl << endl;
         myFile.close();
-    } else {
-        printf("\n##################  'Adding Info into File' failed for some reason dumbass  ##################");
     }
 
-    // File search
 
-    cout << "Enter Thing you wanna search for: " << endl;
-    cin >> search; // chanaged shit here
-    string filename = "pswdHolder.txt"; // File to search
-    displayFileContent("new_test.txt"); // Display content of "new_test.txt"
-    cout << endl; // Output a newline for formatting
+    // New Menu system!!
+    do {
+        // GI for menu
+        cout << endl 
+        << " 1 - Add Password\n"
+        << " 2 - Search\n"
+        << " 3 - Help\n"
+        << " 4 - Exit\n"
+        << " Enter your choice and press return:";
+        // Reads Input
+        cin >> choice;
 
-    string searchStr = search; // String to search
-    searchAndDisplayLineNumbers(filename, searchStr); // Search for the string in the file and display line numbers where it's found
+        switch(choice) {
+            // Code that does what the menu chooses
+            case 1: {
+                // Gets Passwords AND puts them in File
+
+                // Conformation and getting Usernames/Passwords
+                while (check1) {
+                    char aswr = 'n';
+                    char aswr2 = 'n';
+
+                    // Website Name
+                    cout << "Enter Website Name:" << endl;
+                    cin >> website;
+
+                    // Usernames and Passwords
+                    if (aswr == 'n') {
+                        cout << "Type in Username: " << endl;
+                        cin >> usrname;
+                        cout << "Do you confirm this Username [y/n]?" << endl;
+                        cin >> aswr;
+                        if (aswr == 'y') {
+                            cout << "Type in Password: " << endl;
+                            cin >> pswd;
+                            cout << "Do you confirm this Password [y/n]?" << endl;
+                            cin >> aswr2;
+                            if (aswr2 == 'y') {
+                                cout << "Thank you!" << endl;
+                                check1 = false;  // Exit the loop
+                            } else {
+                                cout << "Please confirm the password again." << endl;
+                            }
+                        } else {
+                            cout << "Please confirm the username again." << endl;
+                        }
+                    }
+                }
+
+                // Adding information into file
+                myFile.open("pswdHolder.txt", ios::app);
+                if(myFile.is_open()) {
+                    myFile << website << ":" << usrname << ":" << pswd << endl;
+                    cout << myFile.tellg() << endl;
+                    myFile.close();
+                } else {
+                    printf("\n##################  'Adding Info into File' failed for some reason dumbass  ##################");
+                }
+                bool check1 = true;
+                char aswr = 'n';
+                char aswr2 = 'n';
+                break;
+            }
+            case 2: {
+                // File search
+
+                cout << "Enter Thing you wanna search for: " << endl;
+                cin >> search; // chanaged shit here
+                string filename = "pswdHolder.txt"; // File to search
+                displayFileContent("new_test.txt"); // Display content of "new_test.txt"
+                cout << endl; // Output a newline for formatting
+
+                string searchStr = search; // String to search
+                searchAndDisplayLineNumbers(filename, searchStr); // Search for the string in the file and display line numbers where it's found
+
+                break;
+            }
+            case 3: {
+                
+                break;
+            }
+            case 4: 
+                exit(main());
+            break;
+            
+        }
+        
+
+    } while (choice!=4);
 
     return 0;
 }
